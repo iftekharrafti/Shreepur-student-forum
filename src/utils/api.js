1544a31448225252add6c67466a500de948f9c5d
Iftekhar Rafti <iftekharrafti7040@gmail.com>
2023-12-10 15:59:33 +0600

    Change title dynamically

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const BASE_URL = "https://laravel.amaderthikana.com/api/usasreepur";
 
+export const TITLE = `ইউনিভার্সিটি স্টুডেন্টস্ অ্যাসোসিয়েশন, ঊষা`
+
 export const fetchDataFromApi = async (url, params) => {
   try {
     const { data } = await axios.get(BASE_URL + url, {
